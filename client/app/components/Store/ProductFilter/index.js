5d9068739ad1b958cb3f4726efeb6944f4cfb9a5
GOWTHAMS03 <86407286+GOWTHAMS03@users.noreply.github.com>
2023-05-05 08:29:46 +0530

    first commit

---
+++ client/app/components/Store/ProductFilter/index.js
@@ -0,0 +1,134 @@
+/**
+ *
+ * ProductFilter
+ *
+ */
+
+import { Link } from 'react-router-dom';
+import React from 'react';
+import { Card, CardBody, CardHeader } from 'reactstrap';
+
+import RangeSlider from '../../Common/RangeSlider';
+
+const priceMarks = {
+  1: { label: <p className='fw-normal text-black'>₹1</p> },
+  5000: { label: <p className='fw-normal text-black'>₹5000</p> }
+};
+
+const rateMarks = {
+  0: {
+    label: (
+      <span>
+        <span className='mr-1'>5</span>
+        <i
+          className='fa fa-star fa-1x'
+          style={{ display: 'contents' }}
+          aria-hidden='true'
+        ></i>
+      </span>
+    )
+  },
+  20: {
+    label: (
+      <span>
+        <span className='mr-1'>4</span>
+        <i className='fa fa-star fa-1x' aria-hidden='true'></i>
+      </span>
+    )
+  },
+  40: {
+    label: (
+      <span>
+        <span className='mr-1'>3</span>
+        <i className='fa fa-star fa-1x' aria-hidden='true'></i>
+      </span>
+    )
+  },
+  60: {
+    label: (
+      <span>
+        <span className='mr-1'>2</span>
+        <i className='fa fa-star fa-1x' aria-hidden='true'></i>
+      </span>
+    )
+  },
+  80: {
+    label: (
+      <span>
+        <span className='mr-1'>1</span>
+        <i className='fa fa-star fa-1x' aria-hidden='true'></i>
+      </span>
+    )
+  },
+  100: { label: <span>Any</span> }
+};
+
+const rating = v => {
+  switch (v) {
+    case 100:
+      return 0;
+    case 80:
+      return 1;
+    case 60:
+      return 2;
+    case 40:
+      return 3;
+    case 20:
+      return 4;
+    default:
+      0;
+      return 5;
+  }
+};
+
+const ProductFilter = props => {
+  const { filterProductsm, brands } = props;
+
+  return (
+    <div className='product-filter'>
+      <Card className='mb-4'>
+        <CardHeader tag='h3'>Price</CardHeader>
+        <CardBody>
+          <div className='mx-2 mb-3'>
+            <RangeSlider
+              marks={priceMarks}
+              defaultValue={[1, 2500]}
+              max={5000}
+              onChange={v => {
+                filterProducts('price', v);
+              }}
+            />
+          </div>
+        </CardBody>
+      </Card>
+
+      <Card >
+        <CardHeader tag='h3'>Size</CardHeader>
+        <div className='mini-brand-block'>
+          {brands?.map((brand, index) => (
+            <div className=' d-flex flex-wrap'>
+              <CardBody>
+
+                <Link
+                  to={`/shop/brand/${brand.slug}`}
+                  className='brand-link'
+                  role='menuitem'
+                  onClick={handleMenuItemClick}
+                >
+                  {brand.name}
+                </Link>
+
+              </CardBody>
+
+            </div>
+          ))}
+        </div>
+
+
+
+      </Card>
+    </div>
+  );
+};
+
+export default ProductFilter;
